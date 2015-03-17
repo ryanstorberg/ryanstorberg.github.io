ad2cbae59d70132420fb5c82eae6e971649033bd
Ryan Storberg <ryan@storberg.net>
2015-03-17 15:13:01 -0400

    Initialize 2.0

--- unit1_projects/scripts/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$("html").hide();
-	$("ul").css("margin-top", "1000px");
-	$("ul").animate({marginTop: "0px"}, 5000);
-	$("html").delay(5).fadeIn(5000);
-});
--- unit1_projects/templates/blog_template_v2.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$("header, #content, footer").hide();
-	$("header").delay(10).fadeIn("slow", function(){
-		$("#content, footer").fadeIn("slow");
-	});
-});
