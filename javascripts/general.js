90c49f976dc044bdd286e60124326b5635aac54e
Ryan Storberg <ryan@storberg.net>
2015-03-22 15:49:00 -0400

    Add poster to video tag; increase contrast between nav bar and work list

--- javascripts/general.js
+++ javascripts/general.js
@@ -26,7 +26,7 @@ $(document).on("ready", function() {
 	if(window.mobilecheck()) {
 		$("#fixed-banner").prepend("<img id=\"media-banner\" src=\"images/canyon.jpg\" alt=\"canyon\">");
 	} else {
-		$("#fixed-banner").prepend("<video id=\"media-banner\" autoplay loop><source src=\"videos/canyon.mp4\" type=\"video/mp4\"></video>");
+		$("#fixed-banner").prepend("<video id=\"media-banner\" autoplay loop poster=\"images/canyon.jpg\"><source src=\"videos/canyon.mp4\" type=\"video/mp4\"></video>");
 	}
 
 	$(window).on("scroll", function() {
