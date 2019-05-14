443488543ab2e5e386d2b7d187cccb12e2b56c89
Ryan Storberg <ryan@storberg.xyz>
2019-05-14 14:10:09 -0500

    Misc

--- javascripts/general.js
+++ javascripts/general.js
@@ -1,15 +1,17 @@
-// function adjustHeight(classType) {
-// 	var greatestHeight = 0;
-// 	$(classType).each(function(index, value) {
-// 		var itemHeight = $(value).height();
-// 		console.log(itemHeight);
-// 		if(itemHeight > greatestHeight){
-// 			greatestHeight = itemHeight;
-// 		}
-// 	});
-// 	$(classType).height(greatestHeight);
-// };
-//
-// $(document).on("ready", function() {
-// 	adjustHeight('.service');
-// });
+var currentDayAsNumber = new Date();
+
+var daysAsWords = new Array(7);
+
+daysAsWords[0] =  "Sunday";
+daysAsWords[1] = "Monday";
+daysAsWords[2] = "Tuesday";
+daysAsWords[3] = "Wednesday";
+daysAsWords[4] = "Thursday";
+daysAsWords[5] = "Friday";
+daysAsWords[6] = "Saturday";
+
+var currentDayAsWord = daysAsWords[currentDayAsNumber.getDay()];
+
+function renderCurrentDay() {
+  document.getElementById("today").innerHTML = currentDayAsWord;
+};
